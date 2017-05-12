73e510aec6cab70a39a0d52e91601480aafc5aca
Jenkola <mattyjenks8@gmail.com>
2017-05-12 12:56:12 +0100

    V0.01 Basic model + display todos
    Added base model functions to add, edit, delete, toggle completed for
    todos. Basic console render function added to view.

--- JS/todo_mvc.js
+++ JS/todo_mvc.js
@@ -6,7 +6,38 @@
 
 		model: {
 
-			todos: []
+			todos: [],
+
+			addTodo: function(todoText) {
+
+				this.todos.push({
+					todoText: todoText,
+					completed: false
+				});
+
+				todoApp.view.displayTodos();
+			},
+
+			editTodoText: function(index, updatedText) {
+
+				this.todos[index].todoText = updatedText;
+
+				todoApp.view.displayTodos();
+			},
+
+			deleteTodo: function(index) {
+
+				this.todos.splice(index, 1);
+
+				todoApp.view.displayTodos();
+			},
+
+			toggleCompleted: function(index) {
+
+				this.todos[index].completed = !this.todos[index].completed;
+
+				todoApp.view.displayTodos();
+			}
 			
 		},
 
@@ -16,7 +47,22 @@
 
 		control: {},
 
-		view: {}
+		view: {
+
+			displayTodos: function() {
+
+				var todos = todoApp.model.todos;
+
+				for(var i = 0; i < todos.length; i++) {
+
+					if (todos[i].completed) {
+						console.log('(x) ' + todos[i].todoText);	
+					} else {
+						console.log('( ) ' + todos[i].todoText);
+					}				
+				}
+			}
+		}
 
 	};
 
